6ecddc57180ecb24315870ef791f9d00dc188905
Exegete <exegete@mac.com>
2017-08-02 19:56:22 -0700

    Added an install for oh-my-zsh.

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Installing Oh-My-Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/tmux.conf $HOME/.tmux.conf
 
