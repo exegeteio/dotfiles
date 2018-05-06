4f970963ddbce70c7df04866eaa55afe239a1b8f
Exegete <exegete46@users.noreply.github.com>
2018-05-06 09:36:01 -0700

    Install vim-plug automagically.

--- dotinstall.sh
+++ dotinstall.sh
@@ -3,6 +3,10 @@
 echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "Installing vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Done!  Run :PlugInstall within vim to install plugins."
+
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/tmux.conf $HOME/.tmux.conf
 cat `pwd`/gitconfig >> $HOME/.gitconfig
