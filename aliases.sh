7006fda100d8f724b042be55d501702fbad65a1a
Jack M <root@exegete.me>
2021-02-27 13:28:04 -0700

    Only try to initialize the ssh-agent if ~/.ssh/.

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,15 @@
 # Set up SSH Agent
 # Poached from:  https://code.visualstudio.com/docs/remote/containers#_using-ssh-keys
-if [ -z "$SSH_AUTH_SOCK" ]; then
-  # Check for a currently running instance of the agent
-  RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
-  if [ "$RUNNING_AGENT" = "0" ]; then
-    # Launch a new instance of the agent
-    ssh-agent -s &> ~/.ssh/ssh-agent
+if [ -d "$HOME/.ssh" ]; then
+  if [ -z "$SSH_AUTH_SOCK" ]; then
+    # Check for a currently running instance of the agent
+    RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+    if [ "$RUNNING_AGENT" = "0" ]; then
+      # Launch a new instance of the agent
+      ssh-agent -s &> ~/.ssh/ssh-agent
+    fi
+    eval `cat ~/.ssh/ssh-agent`
   fi
-  eval `cat ~/.ssh/ssh-agent`
 fi
 
 # Preferred editor for local and remote sessions
