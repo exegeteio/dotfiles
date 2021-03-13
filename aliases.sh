76f315e4c9ff5eaae6c265b0f63decfcd3b87b35
Jack M <exegete@hey.com>
2021-03-13 07:11:57 -0700

    Added keychain for ssh-agent management.

--- aliases.sh
+++ aliases.sh
@@ -2,13 +2,17 @@
 # Poached from:  https://code.visualstudio.com/docs/remote/containers#_using-ssh-keys
 if [ -d "$HOME/.ssh" ]; then
   if [ -z "$SSH_AUTH_SOCK" ]; then
-    # Check for a currently running instance of the agent
-    RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
-    if [ "$RUNNING_AGENT" = "0" ]; then
-      # Launch a new instance of the agent
-      ssh-agent -s &> ~/.ssh/ssh-agent
+    if [ -x "$(which keychain)" ]; then
+      eval $(keychain --eval --agents ssh id_rsa)
+    else
+      # Check for a currently running instance of the agent
+      RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+      if [ "$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> ~/.ssh/ssh-agent
+      fi
+      eval `cat ~/.ssh/ssh-agent`
     fi
-    eval `cat ~/.ssh/ssh-agent`
   fi
 fi
 
