a0eda6fc4ded67b9748c74931459d142c003185e
Jack M <exegete@mac.com>
2022-01-09 14:09:10 -0700

    Shellcheck brew.sh

--- brew.sh
+++ brew.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DOTFILES_PATH=`cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd `
+DOTFILES_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 
-if [ -d "$HOME/.brew/Homebrew" ]; then
+if [[ -d "$HOME/.brew/Homebrew" ]]; then
   echo Homebrew already installed.
 else
-  git clone https://github.com/Homebrew/brew $HOME/.brew/Homebrew
-  ln -s $HOME/.brew/Homebrew/bin $HOME/.brew/bin
+  git clone https://github.com/Homebrew/brew "$HOME/.brew/Homebrew"
+  ln -s "$HOME/.brew/Homebrew/bin" "$HOME/.brew/bin"
 fi
 export PATH="$HOME/.brew/bin:$PATH" # Also in zshrc.
-BREW=$(which brew)
+BREW="$(which brew)"
 [[ -x "$BREW" ]] || (echo "Could not find brew after install!"; exit 1)
-$BREW bundle --file=$DOTFILES_PATH/brewfiles/base
+$BREW bundle --file="$DOTFILES_PATH/brewfiles/base"
 
