14099212bc24248c055b0420135043e7b7ae8f99
Jack Mangels <jack.mangels@paynearme.com>
2022-03-07 11:28:27 -0800

    Created installer for vscode settings.

---
+++ vscode/link.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -s "$HOME/.config/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+ln -s "$HOME/.config/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
