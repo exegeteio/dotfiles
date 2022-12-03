eb409e94b5c0fb907fa13e328f178a9caa8f1773
Jack M <exegete@mac.com>
2022-12-02 19:34:57 -0700

    Update vscode settings and link to Insiders.

--- vscode/link.sh
+++ vscode/link.sh
@@ -2,4 +2,6 @@
 
 ln -s "$HOME/.config/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 ln -s "$HOME/.config/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -s "$HOME/.config/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code - Insiders/User/keybindings.json"
+ln -s "$HOME/.config/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code - Insiders/User/settings.json"
 
