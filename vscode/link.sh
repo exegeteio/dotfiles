72739d3b4bda711d1495c5e1d5ed3261301fe4a0
Jack M <exegete@mac.com>
2023-06-02 18:47:47 -0700

    Move vscode to build dir.

--- vscode/link.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s "$HOME/.config/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
-ln -s "$HOME/.config/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-ln -s "$HOME/.config/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code - Insiders/User/keybindings.json"
-ln -s "$HOME/.config/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code - Insiders/User/settings.json"
-
