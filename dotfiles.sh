57dd3e508f0397fa9344886a42a6288f1e34bdd7
Jack M <exegete@mac.com>
2023-06-03 13:31:34 -0700

    Downloads dotfiles during install script.

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,16 +2,6 @@
 
 dotfiles="${DOTFILES_PATH:-${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles}"
 
-if [ ! -e "$dotfiles" ]; then
-  if [ -d "$(dirname $0)/dotfiles" ]; then
-    echo "Linking $(dirname $0) to ${dotfiles}..."
-    mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"
-    ln -s "$(realpath $(dirname $0))" "$dotfiles"
-  else
-    git clone https://github.com/exegeteio/dotfiles.git "$dotfiles"
-  fi
-fi
-
 files=$(ls -1 "${dotfiles}/dotfiles")
 for file in $files; do
   outfile="${HOME}/.$(basename "$file")"
--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-source_path="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+dotfiles="${DOTFILES_PATH:-${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles}"
+if [ ! -e "$dotfiles" ]; then
+  if [ -d "$(dirname $0)/dotfiles" ]; then
+    echo "Linking $(dirname $0) to ${dotfiles}..."
+    mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"
+    ln -s "$(realpath $(dirname $0))" "$dotfiles"
+  else
+    git clone https://github.com/exegeteio/dotfiles.git "$dotfiles"
+  fi
+fi
+
 os=$(uname)
 
-cd "$source_path"
+cd "$dotfiles"
 
 ./dotfiles.sh
 ./brew.sh
 ./build.sh
 [ "$os" == "Linux" ] && sudo ./linux.sh
 
-installer="$source_path/install/$os.sh"
+installer="$dotfiles/install/$os.sh"
 [[ -x "$installer" ]] && bash "$installer"
 
 cd -
