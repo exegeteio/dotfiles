5bb3803d24aa60af445ba4f81313ffa71dfb8cd0
Jack M <jack.mangels@paynearme.com>
2023-11-30 09:19:33 -0800

    Adds copilot and desyncs os clipboard.

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -226,8 +226,9 @@ require('lazy').setup({
     dependencies = {},
   },
   {
-    'github/copilot.nvim',
+    'github/copilot.vim',
     dependencies = {},
+    build = ':Copilot setup'
   },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
@@ -261,7 +262,7 @@ vim.o.mouse = ''
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
