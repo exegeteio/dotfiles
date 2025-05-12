280910a2eb95cf6da6c5529641ad0581973e3bfe
Jack Mangels <jack.mangels@paynearme.com>
2025-05-12 11:00:20 -0700

    Created j.link shortcut, and implemented.

--- bin/j.c
+++ bin/j.c
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-link="$(op read $JIRA_DOMAIN)/secure/QuickSearch.jspa?searchString=$(urlencode $(g.branch))"
+link="$(j.link)"
 out="[$(j.info)](${link})"
 echo "${out}" | pbcopy
 echo "Copied to clipboard: ${out}"
